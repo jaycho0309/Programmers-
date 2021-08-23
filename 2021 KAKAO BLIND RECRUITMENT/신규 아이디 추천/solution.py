9d83b5f5cff639bdc4d7b8734ccb6306565561c8
jaycho0309 <80737266+jaycho0309@users.noreply.github.com>
2021-08-23 16:12:38 +0900

    Update solution.py

--- 2021 KAKAO BLIND RECRUITMENT/신규 아이디 추천/solution.py
+++ 2021 KAKAO BLIND RECRUITMENT/신규 아이디 추천/solution.py
@@ -1,16 +1,18 @@
 def solution(new_id):
     answer = ''
+    # 1
+    new_id = new_id.lower()
     
-    new_id = new_id.lower()  # 1단계
-    
+    # 2
     for i in new_id:
         if i.isalnum() or i in ['-', '_', '.']:
-            answer += i      # 2단계
+            answer += i      
             
+    # 3
     while '..' in answer:
         answer = answer.replace('..', '.')
-                             # 3단계
             
+    # 4
     if answer[0] == '.':
         if len(answer) != 1:
             answer = answer[1:]
@@ -20,17 +22,20 @@ def solution(new_id):
         if len(answer) != 1:
             answer = answer[:-1] 
         else:
-            answer = ''          # 4단계
+            answer = ''
     
+    # 5
     if answer == '':
-        answer = 'a'             # 5단계
-        
+        answer = 'a'
+       
+    # 6
     if len(answer) >= 16:
         answer = answer[:15]
         if answer[-1] == '.':
-            answer = answer[:-1] # 6단계
-            
+            answer = answer[:-1]
+       
+    # 7
     while len(answer) <= 2:
-        answer += answer[-1]     # 7단계
+        answer += answer[-1] 
         
     return answer
